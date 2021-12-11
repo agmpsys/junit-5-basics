24dd15df95f188d9dcc2c14665dc6064f73364ec
adrimartinez <adrian.sys@gmail.com>
2021-12-10 23:39:22 -0300

    More operations and BeforeEach hook.

--- src/test/java/com/lanoqo/MathUtilsTest.java
+++ src/test/java/com/lanoqo/MathUtilsTest.java
@@ -1,14 +1,21 @@
 package com.lanoqo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
+    MathUtils mathUtils;
+
+    @BeforeEach
+    public void init() {
+        mathUtils = new MathUtils();
+    }
+
     @Test
-    void test() {
-        MathUtils mathUtils = new MathUtils();
+    void testAdd() {
         int expected = mathUtils.add(8,2);
         int actual = 10;
 
@@ -16,8 +23,12 @@ class MathUtilsTest {
     }
 
     @Test
+    void testDivedeZeroException() {
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by zero should throw and exception");
+    }
+
+    @Test
     void testgetCircleArea() {
-        MathUtils mathUtils = new MathUtils();
         double expected = mathUtils.getCircleArea(10);
         double actual = 314.15926535897932384626433832795;
         assertEquals(actual, expected, "Get circle area from a radius input.");
