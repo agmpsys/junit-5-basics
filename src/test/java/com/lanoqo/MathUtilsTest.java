25c1bf9bfc3fe422d43e813f518e0cfef604db3c
adrimartinez <adrian.sys@gmail.com>
2021-12-14 18:44:37 -0300

    More operations.

--- src/test/java/com/lanoqo/MathUtilsTest.java
+++ src/test/java/com/lanoqo/MathUtilsTest.java
@@ -1,20 +1,35 @@
 package com.lanoqo;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assumptions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD) //By default
 class MathUtilsTest {
 
     MathUtils mathUtils;
+
+    @BeforeAll
+    static void beforeAllInit() {
+        System.out.println("This need to run before all");
+    }
 
     @BeforeEach
     public void init() {
         mathUtils = new MathUtils();
     }
 
+    @AfterEach
+    void cleanUp() {
+        System.out.println("Cleanning up...");
+    }
+
     @Test
+    @DisplayName("Testig ADD method")
     void testAdd() {
         int expected = mathUtils.add(8,2);
         int actual = 10;
@@ -32,6 +47,37 @@ class MathUtilsTest {
         double expected = mathUtils.getCircleArea(10);
         double actual = 314.15926535897932384626433832795;
         assertEquals(actual, expected, "Get circle area from a radius input.");
+    }
+
+    @Test
+    @DisplayName("TDD method, do not need to run")
+    @Disabled("Work in progress")
+    void testDisabled() {
+        fail();
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testDivedeZeroExceptionLinux() {
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by zero should throw and exception");
+    }
+
+    @Test
+
+    void testDivedeZeroExceptionAssume() {
+        boolean isServerUp = false;
+        assumeTrue(isServerUp);
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by zero should throw and exception");
+    }
+
+    @Test
+    @DisplayName("Multiply method")
+    void testMultiply() {
+        assertAll(
+                () -> assertEquals(4, mathUtils.multiply(2,2)),
+                () -> assertEquals(0, mathUtils.multiply(2, 0)),
+                () -> assertEquals(-2, mathUtils.multiply(2, -1))
+                );
     }
 
 }
