2e695c69492bdb3ffa5d21c429af07faa6957d91
adrimartinez <adrian.sys@gmail.com>
2021-12-16 17:54:02 -0300

    More operations.

--- src/test/java/com/lanoqo/MathUtilsTest.java
+++ src/test/java/com/lanoqo/MathUtilsTest.java
@@ -12,6 +12,8 @@ import org.junit.jupiter.api.condition.OS;
 class MathUtilsTest {
 
     MathUtils mathUtils;
+    TestInfo testInfo;
+    TestReporter testReporter;
 
     @BeforeAll
     static void beforeAllInit() {
@@ -19,8 +21,11 @@ class MathUtilsTest {
     }
 
     @BeforeEach
-    public void init() {
+    public void init(TestInfo testInfo, TestReporter testReporter) {
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
         mathUtils = new MathUtils();
+        testReporter.publishEntry("key", "Running " + testInfo.getDisplayName() + "with tags " + testInfo.getTags());
     }
 
     @AfterEach
@@ -28,25 +33,40 @@ class MathUtilsTest {
         System.out.println("Cleanning up...");
     }
 
-    @Test
-    @DisplayName("Testig ADD method")
-    void testAdd() {
-        int expected = mathUtils.add(8,2);
-        int actual = 10;
+    @Nested
+    @DisplayName("Add method")
+    @Tag("Math")
+    class AddTest {
+        @Test
+        @DisplayName("Testig ADD method for positive")
+        void testAddPositive() {
+            int expected = mathUtils.add(8,2);
+            int actual = 10;
 
-        assertEquals(actual, expected);
+            assertEquals(actual, expected, () -> "Should return right sum");
+        }
+
+        @Test
+        @DisplayName("Testig ADD method for positive")
+        void testAddNegative() {
+            int expected = mathUtils.add(8,-2);
+            int actual = 6;
+
+            assertEquals(actual, expected, () -> "Should return right sum");
+        }
     }
+
 
     @Test
     void testDivedeZeroException() {
-        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by zero should throw and exception");
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), () -> "Divide by zero should throw and exception");
     }
 
-    @Test
+    @RepeatedTest(3)
     void testgetCircleArea() {
         double expected = mathUtils.getCircleArea(10);
         double actual = 314.15926535897932384626433832795;
-        assertEquals(actual, expected, "Get circle area from a radius input.");
+        assertEquals(actual, expected, () -> "Get circle area from a radius input.");
     }
 
     @Test
@@ -59,20 +79,22 @@ class MathUtilsTest {
     @Test
     @EnabledOnOs(OS.LINUX)
     void testDivedeZeroExceptionLinux() {
-        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by zero should throw and exception");
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), () -> "Divide by zero should throw and exception");
     }
 
     @Test
-
     void testDivedeZeroExceptionAssume() {
         boolean isServerUp = false;
         assumeTrue(isServerUp);
-        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), "Divide by zero should throw and exception");
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0), () -> "Divide by zero should throw and exception");
     }
 
     @Test
+    @Tag("Math")
     @DisplayName("Multiply method")
     void testMultiply() {
+        //System.out.println("Running " + testInfo.getDisplayName() + "with tags " + testInfo.getTags());
+        testReporter.publishEntry("Running " + testInfo.getDisplayName() + "with tags " + testInfo.getTags());
         assertAll(
                 () -> assertEquals(4, mathUtils.multiply(2,2)),
                 () -> assertEquals(0, mathUtils.multiply(2, 0)),
